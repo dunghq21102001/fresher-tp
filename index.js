4ab5a399aac363146a2393d65052e9ccf292a748
Dung <dunghq2110@gmail.com>
2024-06-25 10:19:24 +0700

    add some message and merge to release branch

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // first commit, init src
+
+// second commit from dev branch, merged code from release branch to dev branch
+// add some message and push to dev branch
+// create Pr to merge dev branch to release branch
