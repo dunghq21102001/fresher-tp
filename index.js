bace2bd1c50584313984a1b8960623d8e3fb2a94
Dung <dunghq2110@gmail.com>
2024-07-01 10:27:06 +0700

    dev done cherry pick from exam dungqh to dev branch

--- index.js
+++ index.js
@@ -6,11 +6,15 @@
 const a = 'commit 1'
 const b ='commit 2'
 let c = 'commit 3'
-// second commit from dev branch, merged code from release branch to dev branch
-// add some message and push to dev branch
-// create Pr to merge dev branch to release branch
 
+const usingCherryPickWithExamDungQhPracticeAndDevBranch = () => {
+    console.log('Oke dev done');
+}
 
-// second commit from dev branch, merged code from release branch to dev branch
-// add some message and push to dev branch
-// create Pr to merge dev branch to release branch
+usingCherryPickWithExamDungQhPracticeAndDevBranch()
+
+const usingCherryPickWithExamDungQhPracticeAndDevBranch = () => {
+    console.log('Oke dev done');
+}
+
+usingCherryPickWithExamDungQhPracticeAndDevBranch()
