ae963d18ea3af880b9bc56e7f76333315a4c04fb
Dung <dunghq2110@gmail.com>
2024-07-01 10:27:06 +0700

    dev done cherry pick

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 const a = 'commit 1'
 const b ='commit 2'
 let c = 'commit 3'
+
+const usingCherryPickWithExamDungQhPracticeAndDevBranch = () => {
+    console.log('Oke dev done');
+}
+
+usingCherryPickWithExamDungQhPracticeAndDevBranch()
